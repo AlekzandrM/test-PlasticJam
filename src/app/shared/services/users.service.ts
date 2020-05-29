95eb3828a40ff04ae1fa45759b54828720f4d271
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-05-29 21:04:22 +0300

    Users-page.

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -11,7 +11,12 @@ export class UsersService {
   }
 
   fetch(params: any = {}): Observable<Users[]> {
-    return  of(JSON.parse('{"content": [{"id":1,"firstName": "Kri","lastName": "Kra","email": "mail","gender": "men","totalClicks": 1235,"totalPageViews": 57, "ipAddress": "57.13.44"}],"totalPages": 100,"totalElements": 200}'))
+    return  of(JSON.parse('{"content": [' +
+      '{"id":1,"firstName": "Kri","lastName": "Kra","email": "mail","gender": "men","totalClicks": 1235,"totalPageViews": 57, "ipAddress": "57.13.44"},' +
+      '{"id":2,"firstName": "Kri","lastName": "Kra","email": "mail","gender": "men","totalClicks": 1235,"totalPageViews": 57, "ipAddress": "57.13.44"}' +
+      '],' +
+      '"totalPages": 100,"totalElements": 200}')
+    )
 
     // return this.http.get<Users[]>(`/api/v1/users`, {
     //   params: new HttpParams({
--- src/app/users-page/users-page.component.ts
+++ src/app/users-page/users-page.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UsersService} from '../shared/services/users.service';
-import {Observable, Subscription, of} from 'rxjs';
+import { Subscription} from 'rxjs';
 import {UserContent, Users} from '../shared/interfaces';
 import {map} from 'rxjs/operators';
 
@@ -11,24 +11,36 @@ const STEP = 50
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.scss']
 })
-export class UsersPageComponent implements OnInit {
+export class UsersPageComponent implements OnInit, OnDestroy {
+  page1 = 1
+  page2 = 2
+  page3 = 3
+  page4 = 4
+  page5 = 5
 
-  resp: Users[]
   content = []
-  idx: string
+  isActive1 = true
+  isActive2 = false
+  isActive3 = false
+  isActive4 = false
+  isActive5 = false
 
   uSub: Subscription
-  page = 0
+  page = 1
   range = STEP
 
-  totalPages = 0
-  totalElements = 0
+  noMoreUsers = false
 
 
   constructor(private usersService: UsersService) { }
 
   ngOnInit() {
     this.fetch()
+  }
+  ngOnDestroy() {
+    if (this.uSub) {
+      this.uSub.unsubscribe()
+    }
   }
 
   private fetch() {
@@ -47,15 +59,71 @@ export class UsersPageComponent implements OnInit {
         })
       )
       .subscribe((users) => {
-
-      console.log(users)
-      users.map((item) => {
-        this.content.push(item)
-        console.log(this.content)
+        users.map((item) => {
+          console.log(item)
+          this.content.push(item)
+        })
+        this.noMoreUsers = this.content.length < STEP
       })
 
-      })
+  }
 
+  nextPages() {
+    this.page1 += 5
+    this.page2 += 5
+    this.page3 += 5
+    this.page4 += 5
+    this.page5 += 5
+  }
+  previousPages() {
+    if (this.page1 > 1) {
+      this.page1 -= 5
+      this.page2 -= 5
+      this.page3 -= 5
+      this.page4 -= 5
+      this.page5 -= 5
+    } else {
+      return;
+    }
+  }
+  loadNextUsers(event, page) {
+    event.preventDefault()
+    this.page = page
+    this.range = STEP
+    this.content = []
+    this.fetch()
+
+    if (this.page1 === page ) {
+      this.isActive1 = true
+      this.isActive2 = false
+      this.isActive3 = false
+      this.isActive4 = false
+      this.isActive5 = false
+    } else if (this.page2 === page) {
+      this.isActive1 = false
+      this.isActive2 = true
+      this.isActive3 = false
+      this.isActive4 = false
+      this.isActive5 = false
+    } else if (this.page3 === page) {
+      this.isActive1 = false
+      this.isActive2 = false
+      this.isActive3 = true
+      this.isActive4 = false
+      this.isActive5 = false
+    } else if (this.page4 === page) {
+      this.isActive1 = false
+      this.isActive2 = false
+      this.isActive3 = false
+      this.isActive4 = true
+      this.isActive5 = false
+    } else {
+      this.isActive1 = false
+      this.isActive2 = false
+      this.isActive3 = false
+      this.isActive4 = false
+      this.isActive5 = true
+    }
   }
 
 }
